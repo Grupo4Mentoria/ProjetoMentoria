791ee8048b405df3d11eec3263dd2c1eadbc5713
Thiago <thiagovalentim@hotmail.com.br>
2023-08-31 19:49:33 -0300

    Base project starded with cocoapods

---
+++ ProjetoMentoria/Pods/Target Support Files/Pods-ProjetoMentoria-ProjetoMentoriaUITests/Pods-ProjetoMentoria-ProjetoMentoriaUITests-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_ProjetoMentoria_ProjetoMentoriaUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_ProjetoMentoria_ProjetoMentoriaUITestsVersionString[];
+
